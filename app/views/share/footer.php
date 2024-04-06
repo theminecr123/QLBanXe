40572d7ef2af38aec55b569a5f3b380be910cdae
Huỳnh Vĩnh Tiến <dmx031117@gmail.om>
2024-04-06 13:11:31 +0700

    Tien design giao dien: List product, cart, order history, order detail

--- app/views/share/footer.php
+++ app/views/share/footer.php
@@ -1,6 +1,6 @@
 
   <!-- Footer -->
-            <footer class="sticky-footer bg-white">
+            <footer style="margin-top:200px;" class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
                         <span>Copyright &copy; Your Website 2021</span>
--- app/views/share/header.php
+++ app/views/share/header.php
@@ -17,7 +17,7 @@
 
         <!-- Custom styles for this template-->
         <link href="/QLBanXe/public//css/sb-admin-2.min.css" rel="stylesheet">
-
+        <link rel="stylesheet" type="text/css" href="../public/css/style.css">
     </head>
 
     <body id="page-top">
@@ -188,8 +188,14 @@
                                 <!-- Nav Item - Cart -->
                                 <li class="nav-item">
                                     <a class="nav-link" href="/QLBanXe/cart/show" style="font-size: 20px;">
-                                        <i class="fas fa-shopping-cart fa-fw"></i>
-                                        <span class="badge badge-danger badge-counter">Xem giỏ hàng</span>
+                                        <!-- <i class="fas fa-shopping-cart fa-fw"></i>
+                                        <span class="badge badge-danger badge-counter">Xem giỏ hàng</span> -->
+                                        <button class="CartBtn">
+                                        <span class="IconContainer"> 
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+                                        </span>
+                                        <p class="text-cart">Giỏ Hàng</p>
+                                        </button>
                                     </a>
                                 </li>
                                 <!-- Other topbar items ... -->
@@ -354,7 +360,7 @@
 
                                 <?php
                                 if(isset($_SESSION['name'])){
-                                    echo "<a href='/QLBanXe/account/logout'>Logout</a>";
+                                    echo "<a class='button__logout' href='/QLBanXe/account/logout'>Đăng Xuất</a>";
                                 }
                                 ?>
 
