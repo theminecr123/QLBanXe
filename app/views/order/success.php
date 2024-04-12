23a87b8c52532bc931effada9436bf938725654b
Huỳnh Vĩnh Tiến <dmx031117@gmail.om>
2024-04-12 09:43:23 +0700

    Tien push

--- app/views/order/success.php
+++ app/views/order/success.php
@@ -8,9 +8,9 @@
     <title>Success</title>
 </head>
 <body>
-    <p>Đặt hàng thành công!</p>
-    
-    <a href="showOrderHistory" class="btn btn-primary">Xem lịch sử đơn hàng</a>
+    <p class="order-success">Đặt hàng thành công!</p>
+    <img style="width: 200px;margin-left: 500px; padding-bottom: 20px;" src="https://cdn1.iconfinder.com/data/icons/toolbar-signs/512/OK-512.png">
+    <a href="showOrderHistory" class="btn btn-primary" style="margin-left: 520px;">Xem lịch sử đơn hàng</a>
 </body>
 </html>
 
